3ea49cf0ff1dff8c52e5ecec3df11995877c6e22
thinkgarden <thinkgarden1990@163.com>
2015-12-12 11:14:06 +0800

    wip

--- js/common.js
+++ js/common.js
@@ -1,7 +1,21 @@
 $(function  () {
   $(".cd-header .cd-main-nav").on("click",function (event) {
     if($(event.target).is('.cd-header .cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
-  })
+  });
+  // 向上滚动，显示菜单栏
+  var minWidth = 1170;
+  if ($(window).width() > minWidth) {
+      var navHeight = $(".cd-header").height();
+      $(window).on("scroll", {
+          previousTop: 0
+      },
+      function() {
+          var scroll_top = $(window).scrollTop();
+          // 只要向上滚动scroll_top就会小于previousTop
+          scroll_top < this.previousTop ? scroll_top > 0 && $(".cd-header").hasClass("is-fixed") ? $(".cd-header").addClass("is-visible") : $(".cd-header").removeClass("is-visible is-fixed") : ($(".cd-header").removeClass("is-visible"), scroll_top > navHeight && !$(".cd-header").hasClass("is-fixed") && $(".cd-header").addClass("is-fixed")),
+          this.previousTop = scroll_top;
+      })
+  }
 })
 
 function chkChinese(str) {
