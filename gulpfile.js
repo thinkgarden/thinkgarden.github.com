5d172617538ffe077e8fc28652069057ee4abaae
thinkgarden <thinkgarden1990@163.com>
2015-12-22 17:37:09 +0800

    change style for post

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+gulp.task('imagemin', function(){
+  return gulp.src('images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('images'));
+});
 
 gulp.task('sass',function () {
   gulp.src('styles/*.scss')
       .pipe(sass())
       .pipe(autoprefixer())
+      .pipe(minify())
       .pipe(gulp.dest('css'))
 });
 
